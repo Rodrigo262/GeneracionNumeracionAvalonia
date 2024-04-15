b134469a2f7752c8874c6963d2490303ae5f77f8
Rodrigo <rodrigodhv@gmail.com>
2024-04-15 18:25:57 +0200

    Adapt packaging script

--- package/MacOs/packaging.sh
+++ package/MacOs/packaging.sh
@@ -1,29 +1,39 @@
-#cleanup folders
-APP_NAME="/Users/rodrigo/Documents/Rodrigo/Git/GeneracionNumeracionAvaloniaDesktop/App/GeneracionNumeracion.app"
-PUBLISH_OUTPUT_DIRECTORY="/Users/rodrigo/Documents/Rodrigo/Git/GeneracionNumeracionAvaloniaDesktop/GeneracionNumeracionAvalonia/bin/Release/net8.0/osx-arm64/publish/."
-PROJECT="/Users/rodrigo/Documents/Rodrigo/Git/GeneracionNumeracionAvaloniaDesktop/GeneracionNumeracionAvalonia/GeneracionNumeracionAvalonia.csproj"
-INFO_PLIST="Info.plist"
-ICON_FILE="Counter.icns"
+#!/bin/bash
 
-#cleanup folders
-rm -rf "$APP_NAME/Contents/MacOS/" 
-rm -rf "$APP_NAME/Contents/CodeResources" 
-rm -rf "$APP_NAME/Contents/_CodeSignature" 
-rm -rf "$APP_NAME/Contents/embedded.provisionprofile" 
-mkdir -p "$APP_NAME/Contents/Frameworks/"
-mkdir "$APP_NAME/Contents/Resources"
+# Definir las rutas relativas
+RELATIVE_ROOT=$(pwd)
+RELATIVE_APP_NAME="$RELATIVE_ROOT/App/GeneracionNumeracion.app"
+RELATIVE_PUBLISH_OUTPUT_DIRECTORY="$RELATIVE_ROOT/GeneracionNumeracionAvalonia/bin/Release/net8.0/osx-arm64/publish/."
+RELATIVE_PROJECT="$RELATIVE_ROOT/GeneracionNumeracionAvalonia/GeneracionNumeracionAvalonia.csproj"
+INFO_PLIST="$RELATIVE_ROOT/package/Macos/Info.plist"
+ICON_FILE="$RELATIVE_ROOT/package/Macos/Counter.icns"
 
-#Build app
-dotnet publish "$PROJECT" -c Release -f net8.0 -r osx-arm64 --self-contained=true -p:PublishSingleFile=true -p:UseAppHost=true
 
-#Move app
-mkdir -p "$APP_NAME/Contents/MacOS/"
-cp -R -f "$PUBLISH_OUTPUT_DIRECTORY"/* "$APP_NAME/Contents/MacOS/"
+echo "$ABSOLUTE_PATH"
+echo "$RELATIVE_APP_NAME"
+echo "$RELATIVE_PUBLISH_OUTPUT_DIRECTORY"
+echo "$RELATIVE_PROJECT"
+echo "$INFO_PLIST"
+echo "$ICON_FILE"
 
-echo "$PUBLISH_OUTPUT_DIRECTORY"
-rm -rf "$APP_NAME/Contents/MacOS/publish"
-cp "$INFO_PLIST" "$APP_NAME/Contents/Info.plist"
-cp "$ICON_FILE" "$APP_NAME/Contents/Resources/$ICON_FILE"
+# Limpiar carpetas
+rm -rf "$RELATIVE_APP_NAME/Contents/MacOS/"
+rm -rf "$RELATIVE_APP_NAME/Contents/CodeResources"
+rm -rf "$RELATIVE_APP_NAME/Contents/_CodeSignature"
+rm -rf "$RELATIVE_APP_NAME/Contents/embedded.provisionprofile"
+mkdir -p "$RELATIVE_APP_NAME/Contents/Frameworks/"
+mkdir "$RELATIVE_APP_NAME/Contents/Resources"
 
-echo "[INFO] Creating GeneracionNumeracion.pkg"
-productbuild --component "$APP_NAME" /Applications "/Users/rodrigo/Documents/Rodrigo/Git/GeneracionNumeracionAvaloniaDesktop/GeneracionNumeracion.pkg"
+# Compilar la aplicación
+dotnet publish "$RELATIVE_PROJECT" -c Release -f net8.0 -r osx-arm64 --self-contained=true -p:PublishSingleFile=true -p:UseAppHost=true
+
+# Mover la aplicación
+mkdir -p "$RELATIVE_APP_NAME/Contents/MacOS/"
+cp -R -f "$RELATIVE_PUBLISH_OUTPUT_DIRECTORY"/* "$RELATIVE_APP_NAME/Contents/MacOS/"
+rm -r "$RELATIVE_APP_NAME/Contents/MacOS"/*.pdb
+rm -rf "$RELATIVE_APP_NAME/Contents/MacOS/publish"
+cp "$INFO_PLIST" "$RELATIVE_APP_NAME/Contents/"
+cp "$ICON_FILE" "$RELATIVE_APP_NAME/Contents/Resources/"
+
+# echo "[INFO] Creando GeneracionNumeracion.pkg"
+productbuild --component "$RELATIVE_APP_NAME" /Applications "$RELATIVE_ROOT/GeneracionNumeracion.pkg"
