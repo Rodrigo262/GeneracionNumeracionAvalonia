8b1b368e3386c5edad0681caf18d50f4bf15a0c6
Rodrigo <rodrigodhv@gmail.com>
2024-04-15 13:21:07 +0200

    Initial commit

---
+++ GeneracionNumeracionAvalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace GeneracionNumeracionAvalonia.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ GeneracionNumeracionAvalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace GeneracionNumeracionAvalonia.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
